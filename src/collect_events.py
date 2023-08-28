bc74e77f51af18eb8b5b93c5ee8074b2d9671ad4
rafalum <de.rafael@pm.me>
2023-08-28 11:19:13 +0200

    clean up

--- src/collect_events.py
+++ src/collect_events.py
@@ -3,33 +3,31 @@ import json
 import math
 import time
 from web3 import Web3
-from utils import get_contract
 
-USDC_ETH_POOL_ADDRESS = "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640"
+from src.utils import get_contract
 
-contract = get_contract()
+def collect_events(contract, from_block, to_block, events=["Swap", "Mint", "Burn"]):
 
-event_names = ["Swap", "Mint", "Burn"]
+    increment = min(2000, to_block - from_block)
+    for i in range(from_block, to_block, increment):
 
-from_block = 17000001
-to_block = 17020000
-increment = 2000
+        print(f"{i}/{to_block}")
 
-for i in range(from_block, to_block, increment):
+        for event_name in events:
 
-    print(f"{i}/{to_block}")
+            event_filter = contract.events[event_name].create_filter(fromBlock=i, toBlock=i+increment)
 
-    for event_name in event_names:
+            # get all events that match the filter
+            event_data = event_filter.get_all_entries()
 
-        event_filter = contract.events[event_name].create_filter(fromBlock=i, toBlock=i+increment)
+            for event in event_data:
+                with open(f"data/{event_name}.csv", "a") as f:
+                    if event_name == "Swap":
+                        f.writelines(f"{event.blockNumber}, {event.args['tick']}, {event.args['liquidity']}, {event.args['sqrtPriceX96']}, {event.args['amount0']}, {event.args['amount1']}\n")
+                    else:
+                        f.writelines(f"{event.blockNumber}, {event.args['tickLower']}, {event.args['tickUpper']}, {event.args['amount0']}, {event.args['amount1']}\n")
 
-        # get all events that match the filter
-        events = event_filter.get_all_entries()
 
-        for event in events:
-            with open(f"data/{event_name}.csv", "a") as f:
-                if event_name == "Swap":
-                    f.writelines(f"{event.blockNumber}, {event.args['tick']}, {event.args['liquidity']}, {event.args['sqrtPriceX96']}, {event.args['amount0']}, {event.args['amount1']}\n")
-                else:
-                    f.writelines(f"{event.blockNumber}, {event.args['tickLower']}, {event.args['tickUpper']}, {event.args['amount0']}, {event.args['amount1']}\n")
-            
+if __name__ == "__main__":
+    contract = get_contract()
+    collect_events(contract, 17000001, 17500001)
